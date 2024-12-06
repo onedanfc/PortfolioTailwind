aa5d84400f07b98acce1e7a5fde5ee9a21e9ad50
Bayu <onedanfc@gmail.com>
2024-12-06 07:50:13 +0700

    fix bug

--- src/login.php
+++ src/login.php
@@ -16,7 +16,7 @@ if ($username === $valid_username && $password === $valid_password) {
     setcookie('user', $username, time() + 120, "/");
 
     // Redirect to dashboard
-    header("Location: dashboard.php");
+    header("Location: dashboard.html");
     exit();
 } else {
     echo "Invalid username or password.";
